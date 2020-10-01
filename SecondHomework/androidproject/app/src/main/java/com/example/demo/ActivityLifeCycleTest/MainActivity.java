cb4a3264bc1c61b2a9253b68eee714986c0c4e4b
DESKTOP-2MK65FR\MaiBenBen <m13192313307@163.com>
2020-10-01 23:33:13 +0800

    活动的启动模式

---
+++ SecondHomework/androidproject/app/src/main/java/com/example/demo/ActivityLifeCycleTest/MainActivity.java
@@ -0,0 +1,137 @@
+package com.example.demo.ActivityLifeCycleTest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.demo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG ="MainActivity";
+
+    public MainActivity() {
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.add_item:
+                Toast.makeText(this,"You clicked Add",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this,"You clicked remove",Toast.LENGTH_SHORT).show();
+                break;
+                default:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG,"onCreate");
+
+        if(savedInstanceState != null){
+            String tempData = savedInstanceState.getString("data_key");
+            Log.d(TAG,tempData);
+        }
+
+        Button startNormalActivity = (Button) findViewById(R.id.start_normal_activity);
+        Button startDialogActivity = (Button) findViewById(R.id.start_dialog_activity);
+
+        startNormalActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, NormalActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        startDialogActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, DialogActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+//        Button button1 = (Button) findViewById(R.id.button2);
+//        button1.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                String data = "Hello SecondActivity";
+//                Intent intent= new Intent(MainActivity.this,SecondActivity.class);
+//                intent.putExtra("extra_data",data);
+//                startActivity(intent);
+//            }
+            //            @Override
+//            public void onClick(View view) {
+//                Toast.makeText(MainActivity.this,"You clicked button 1",
+//                        Toast.LENGTH_SHORT).show();
+//                Log.d("MainActivity","kkkkk");
+//            }
+//        });
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG,"onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG,"onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG,"onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG,"onDestory");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG,"onRestart");
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState, @NonNull PersistableBundle outPersistentState) {
+        super.onSaveInstanceState(outState, outPersistentState);
+        String tempData = "Something you just typed";
+        outState.putString("data_key",tempData);
+    }
+}
+
+
+
