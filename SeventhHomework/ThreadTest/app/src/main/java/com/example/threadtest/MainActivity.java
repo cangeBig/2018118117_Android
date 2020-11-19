5185e67c2bd8e000489cc935b6151d8350795468
DESKTOP-2MK65FR\MaiBenBen <m13192313307@163.com>
2020-11-19 19:44:17 +0800

    数据交换

---
+++ SeventhHomework/ThreadTest/app/src/main/java/com/example/threadtest/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.threadtest;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    public static final int UPDATE_TEXT=1;
+    private TextView text;
+    private Handler handler = new Handler(){
+        @Override
+        public void handleMessage( Message msg) {
+            switch (msg.what){
+                case UPDATE_TEXT:
+                    text.setText("Nice to meet you");
+                    break;
+                 default:
+                     break;
+            }
+        }
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text = (TextView) findViewById(R.id.textone);
+        Button changeText = (Button) findViewById(R.id.change_text);
+        changeText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.change_text:
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Message message = new Message();
+                        message.what = UPDATE_TEXT;
+                        handler.sendMessage(message);
+                    }
+                }).start();
+                break;
+            default:
+                break;
+        }
+    }
+}
